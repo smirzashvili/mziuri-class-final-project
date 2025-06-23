b5cb66431d00129e9be75125806e17393f3bc884
Mr-Saba <mirzashvili.saba@gmail.com>
2025-06-23 16:10:26 +0400

    .

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,7 @@ app.use(helmet({
       mediaSrc: ["'self'", "*"],
       videoSrc: ["'self'", "*"],
       imgSrc: ["'self'", "*", "data:"],  // note: no quotes around data: here
+      connectSrc: ["'self'", "http://localhost:3003"],
     }
   }
 }));
