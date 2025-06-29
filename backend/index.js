e24e51f4386343b5ee5abdc2da3548b9b01ebd3d
Mr-Saba <mirzashvili.saba@gmail.com>
2025-06-29 17:01:33 +0400

    ..

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173", "https://demo-melodymatch.onrender.com"], // React frontend
     credentials: true
-  }
+  } 
 });
 
 dotenv.config()
