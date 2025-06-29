df4815f3ce952afdd279fd5d1862564c6897b5ed
Mr-Saba <mirzashvili.saba@gmail.com>
2025-06-29 16:26:22 +0400

    little mobile design updates

--- frontend/src/layouts/mobile/BottomBar.jsx
+++ frontend/src/layouts/mobile/BottomBar.jsx
@@ -4,10 +4,12 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Discover from '../../assets/icons/discover.svg';
 import Messages from '../../assets/icons/messages.svg';
 import Profile from '../../assets/icons/profile.svg';
+import { useChatData } from '../../context/ChatNotificationsContext.jsx';
 
 function BottomBar() {
   const location = useLocation();
   const currentPath = location.pathname;
+  const { unreadMessagesCount } = useChatData();
 
   return (
     <div className='bottomBar'>
@@ -31,6 +33,10 @@ function BottomBar() {
             size={"calc(16px * var(--app-scale))"}
           />
           <p>Messages</p>
+          {
+            unreadMessagesCount > 0 &&
+            <p className={`unreadMessagesCount`}>{unreadMessagesCount}</p>
+          }
         </Link>
         <Link
           to="/profile"
